58853e0065b1c3fd0b3c0e592ce1785eeb602d0d
MapleSugar365 <steady.i@foxmail.com>
2025-07-10 15:34:23 +0800

    Fallback port to 1.20.1 (Neo) Forge

--- src/main/java/com/maplesugar365/peaceful_depletion/PeacefulDepletion.java
+++ src/main/java/com/maplesugar365/peaceful_depletion/PeacefulDepletion.java
@@ -1,23 +1,37 @@
 package com.maplesugar365.peaceful_depletion;
 
-import net.neoforged.fml.common.Mod;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
-import net.neoforged.neoforge.network.registration.PayloadRegistrar;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.simple.SimpleChannel;
 import com.maplesugar365.peaceful_depletion.network.Data;
 
 @Mod(PeacefulDepletion.MODID)
 public class PeacefulDepletion {
     public static final String MODID = "peaceful_depletion";
+    private static final String PROTOCOL_VERSION = "1.0.2";
+    public static SimpleChannel CHANNEL;
 
-    public PeacefulDepletion(IEventBus modEventBus) {
-        modEventBus.addListener(this::registrarDataChannel);
+    public PeacefulDepletion() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
     }
 
-    @SuppressWarnings("null")
-    private void registrarDataChannel(RegisterPayloadHandlersEvent event) {
-        final PayloadRegistrar registrar = event.registrar(MODID)
-                .versioned("1.0.2");
-        registrar.playToClient(Data.TYPE, Data.STREAM_CODEC, null);
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+            CHANNEL = NetworkRegistry.ChannelBuilder
+                    .named(new ResourceLocation(MODID, "data_channel"))
+                    .networkProtocolVersion(() -> PROTOCOL_VERSION)
+                    .clientAcceptedVersions(PROTOCOL_VERSION::equals)
+                    .serverAcceptedVersions(PROTOCOL_VERSION::equals)
+                    .simpleChannel();
+            CHANNEL.registerMessage(0, Data.class,
+                    Data::encode,
+                    Data::decode,
+                    Data::handle);
+        });
     }
 }
--- src/main/java/com/maplesugar365/peaceful_depletion/network/Data.java
+++ src/main/java/com/maplesugar365/peaceful_depletion/network/Data.java
@@ -1,20 +1,32 @@
 package com.maplesugar365.peaceful_depletion.network;
 
-import io.netty.buffer.ByteBuf;
-import net.minecraft.network.codec.ByteBufCodecs;
-import net.minecraft.network.codec.StreamCodec;
-import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraftforge.network.NetworkEvent;
+import java.util.function.Supplier;
 
-public record Data(int data) implements CustomPacketPayload {
-    public static final CustomPacketPayload.Type<Data> TYPE = new CustomPacketPayload.Type<>(
-            ResourceLocation.fromNamespaceAndPath("peaceful_depletion", "data_channel"));
-    public static final StreamCodec<ByteBuf, Data> STREAM_CODEC = StreamCodec.composite(
-            ByteBufCodecs.VAR_INT,
-            Data::data,
-            Data::new);
+public class Data {
+    private final int data;
 
-    public CustomPacketPayload.Type<? extends CustomPacketPayload> type() {
-        return TYPE;
+    public Data(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void encode(FriendlyByteBuf buffer) {
+        buffer.writeInt(data);
+    }
+
+    public static Data decode(FriendlyByteBuf buffer) {
+        return new Data(buffer.readInt());
+    }
+
+    public void handle(Supplier<NetworkEvent.Context> contextSupplier) {
+        NetworkEvent.Context context = contextSupplier.get();
+        context.enqueueWork(() -> {
+        });
+        context.setPacketHandled(true);
     }
 }
